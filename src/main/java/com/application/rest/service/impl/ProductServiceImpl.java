f9b262cefe01ba6e383b77a4d97f8721ac4d7514
Henry Niama <hsniama@utpl.edu.ec>
2023-12-23 19:27:49 -0500

    Capa de Servicio.

---
+++ src/main/java/com/application/rest/service/impl/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.application.rest.service.impl;
+
+import com.application.rest.entities.Product;
+import com.application.rest.persistence.iProductDAO;
+import com.application.rest.service.iProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements iProductService {
+
+    @Autowired
+    private iProductDAO productDAO;
+
+    @Override
+    public List<Product> findAll() {
+        return productDAO.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productDAO.findById(id);
+    }
+
+    @Override
+    public List<Product> findByPriceInRange(BigDecimal min, BigDecimal max) {
+        return productDAO.findByPriceInRange(min, max);
+    }
+
+    @Override
+    public void save(Product product) {
+        productDAO.save(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productDAO.deleteById(id);
+    }
+}
